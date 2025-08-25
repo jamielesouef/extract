5d831ef75d16cf8399d7bf2f6088ce677ad5068e
Jamie Le Souef <jamielesouef@icloud.com>
2025-08-25 10:22:23 +1000

    Initial Commit

---
+++ extract/extractApp.swift
@@ -0,0 +1,17 @@
+//
+//  extractApp.swift
+//  extract
+//
+//  Created by Jamie Le Souef on 25/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct extractApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
